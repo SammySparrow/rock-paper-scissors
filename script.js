a29a4239b9bb33b3d85dfd6b803649b53340652b
SammySparrow <217705499+SammySparrow@users.noreply.github.com>
2025-07-29 19:41:50 +1000

    Create computer choice function
    Implemented logic for function getComputerChoice,
    tested with console log

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3);
+  let result;
+
+  if (choice === 0) {
+    return (result = "rock");
+  } else if (choice === 1) {
+    return (result = "paper");
+  } else {
+    return (result = "scissors");
+  }
+}
+
+console.log(getComputerChoice());
