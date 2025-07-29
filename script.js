6e27a0f44034bde875dbafff5a25cd3bbcf42a32
SammySparrow <217705499+SammySparrow@users.noreply.github.com>
2025-07-29 20:59:38 +1000

    Created round function
    Implemented logic for handling an individual round.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
   let result;
@@ -11,4 +14,43 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+  return prompt("Select rock, paper, or scissors");
+}
+
+function playRound(humanChoice, computerChoice) {
+  let humanChoiceLower = humanChoice.toLowerCase();
+  let result;
+
+  if (
+    (humanChoiceLower === "rock" && computerChoice === "rock") ||
+    (humanChoiceLower === "paper" && computerChoice === "paper") ||
+    (humanChoiceLower === "scissors" && computerChoice === "scissors")
+  ) {
+    result = "Draw.";
+  } else if (
+    (humanChoiceLower === "rock" && computerChoice === "scissors") ||
+    (humanChoiceLower === "paper" && computerChoice === "rock") ||
+    (humanChoiceLower === "scissors" && computerChoice === "paper")
+  ) {
+    result = "You win!";
+  } else {
+    result = "You lose...";
+  }
+
+  if (result === "You win!") {
+    humanScore++;
+  } else if (result === "You lose...") {
+    computerScore++;
+  }
+
+  console.log(`You chose ${humanChoiceLower}, computer chose ${computerChoice}. ${result}
+    The current score is:
+    Player - ${humanScore}
+    Computer - ${computerScore}`);
+}
+
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
